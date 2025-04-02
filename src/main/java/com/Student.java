5c4e27579e04249819685f5f1c6e32711777651c
DESKTOP-3DOP2EC\ADMIN <rinkusalunke9@gmail.com>
2025-04-03 00:00:09 +0530

    1st commit

---
+++ src/main/java/com/Student.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Student {
+
+	public Student() {
+		 System.out.println("This is a Student Class Constructor");
+	}
+	
+	public void display() {
+		System.out.println("Display Method Call");
+	}
+	
+}
---
+++ src/main/java/com/Test.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springFramework.context.ApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext ac=
+		
+		
+		
+		
+	}
+
+}
